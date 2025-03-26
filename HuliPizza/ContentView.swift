b38e99dd11e44371d2836cc2aa063ea7c92eede8
ar16026 <anthony.rubbo@jetblue.com>
2025-03-26 14:57:48 -0400

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Anthony Rubbo on 3/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Anthony Rubbo on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
